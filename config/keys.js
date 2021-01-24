9bb7dc19d2e1b7e205b7afafe39ed1793917a175
Vigneshraj77 <chitraselva33@gmail.com>
2021-01-24 20:30:14 +0530

    Login and reg. backend

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:  "mongodb+srv://User1:quizpass@cluster0.kx5xe.mongodb.net/quiz-login?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+
+app.use(bodyParser.json());
+
+const dbURL =  "mongodb+srv://User1:quizpass@cluster0.kx5xe.mongodb.net/quiz-login?retryWrites=true&w=majority";
+
+//connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB_URI || dbURL,
+    { useUnifiedTopology:true, useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>console.log(`Server up and running on port ${port}`));
