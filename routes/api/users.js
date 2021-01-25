7f85d25a5c5f4e404b377aff4dcf0b129b665eff
Vigneshraj77 <chitraselva33@gmail.com>
2021-01-25 14:20:47 +0530

    bug fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -48,13 +48,6 @@ router.post("/register", (req, res) => {
 
 router.post("/login",(req,res) => {
 
-    //Form Valdiation
-    const {errors, isValid} = validateLoginInput(req.body);
-
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
     const email = req.body.email;
     const password = req.body.password;
    
